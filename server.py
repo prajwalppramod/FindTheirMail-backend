0373ca15c2d9f6fcb81de913ea55fec0d7c714c8
prajwalppramod <prajwalppramod@gmail.com>
2024-02-27 08:55:40 +0530

    fix:cors front-end linked

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/generate_emails": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/generate_emails": {"origins": "https://findtheirmail.vercel.app/"}})
 
 # Define your routes and functions as usual
 
